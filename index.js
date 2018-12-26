e83fb913c447b95de85534eedef155221bc28831
Alexander Grigoriev <grigoriev@megaputer.ru>
2018-12-26 12:12:48 +0300

    Reduce and simplify loader

--- index.js
+++ index.js
@@ -5,56 +5,59 @@ const loaderUtils = require('loader-utils');
 
 /** @type {import('webpack').loader.Loader} */
 module.exports = function(content) {
-  this.cacheable();
+  this.cacheable && this.cacheable();
+
+  const options = loaderUtils.getOptions(this) || {};
   const callback = this.async();
-  const options = loaderUtils.getOptions(this);
-  genTypings(this.resourcePath, content, options, callback);
+
+  let typings = '';
+
+  if (options.banner) {
+    typings = `${options.banner}\n`;
+  }
+
+  {
+    let classes = getClasses(content);
+
+    if (options.namedExport) {
+      for (let c of classes) {
+        typings += `export const ${c}: string;\n`;
+      }
+    } else {
+      const i = getInterfaceName(this.resourcePath);
+      typings += `export interface ${i} {\n`;
+      for (let c of classes) {
+        typings += `  '${c}': string\n`;
+      }
+      typings += `}\ndeclare const styles: ${i};\nexport default styles;\n`;
+    }
+  }
+
+  fs.writeFileSync(getDtsPath(this.resourcePath), typings);
+
+  callback(null, content);
 };
 
 /**
- * @param {string} [path]
  * @param {string | Buffer} [content]
- * @param {import('loader-utils').OptionObject} [options]
- * @param {import('webpack').loader.loaderCallback} [callback]
  */
-function genTypings(path, content, options, callback) {
+function getClasses(content) {
   if (Buffer.isBuffer(content)) {
     content = content.toString('utf8');
   }
 
   /** @type {string[]} */
   let classes = [];
-  {
-    /** @type {RegExpExecArray} */
-    let match;
-    while (match = classesRegex.exec(content)) {
-      if (classes.indexOf(match[1]) === -1) {
-        classes.push(match[1]);
-      }
+
+  /** @type {RegExpExecArray} */
+  let match;
+  while (match = classesRegex.exec(content)) {
+    if (classes.indexOf(match[1]) === -1) {
+      classes.push(match[1]);
     }
   }
 
-  let typings = '';
-  {
-    if (options.banner) {
-      typings = `${options.banner}\n`;
-    }
-    if (options.namedExport) {
-      for (let c of classes) {
-        typings += `export const ${c}: string;\n`;
-      }
-    } else {
-      const name = getInterfaceName(path);
-      typings += `export interface ${name} {\n`;
-      for (let c of classes) {
-        typings += `  '${c}': string\n`;
-      }
-      typings += `}\ndeclare const styles: ${name};\nexport default styles;\n`;
-    }
-  }
-  writeFile(getDtsPath(path), typings);
-
-  callback(null, content);
+  return classes;
 }
 
 const classesRegex = /"([^"\\]+)":/g;
@@ -73,14 +76,4 @@ function getInterfaceName(path) {
   return fp.basename(path)
     .replace(/^(\w)/, (_, c) => 'I' + c.toUpperCase())
     .replace(/\W+(\w)/g, (_, c) => c.toUpperCase());
-}
-
-/**
- * @param {string} [path]
- * @param {string} [content]
- */
-function writeFile(path, content) {
-  if (!fs.existsSync(path) || fs.readFileSync(path).toString('utf8') !== content) {
-    fs.writeFileSync(path, content);
-  }
 }
